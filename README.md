e889b90df4160ca203849b9c5b5038e881663e72
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2018-06-16 09:43:26 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mochi-town
+
+## Credits
+
+[Bulma](https://bulma.io/) - Bulma is an open source CSS framework based on Flexbox.
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <hello-world msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
@@ -11,7 +11,7 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    'hello-world': HelloWorld,
   },
 })
 export default class Home extends Vue {}
